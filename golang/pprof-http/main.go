c2a0906a3a8c9bb02af56006f7b2028fbb5ba553
Mizue Ando <mzeand33@gmail.com>
2024-06-27 22:54:35 +0900

    WIP

--- golang/pprof-http/main.go
+++ golang/pprof-http/main.go
@@ -17,7 +17,6 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World from Go. %v\n", time.Now())
 	fmt.Fprintf(w, "%s", readFile("test.txt"))
-
 }
 
 func readFile(fileName string) string {
